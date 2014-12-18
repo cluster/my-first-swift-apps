2fe29acbaa95b88f01942532684b1b17fd7bc5d2
Clément Uster <clement.uster@gmail.com>
2014-12-18 20:53:44 +0100

    first swift commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "clm"
+var a = 12
+var b = 23
+
+var output = "toto"+str+" \(str) \(a) * \(b) = \(a*b)"
--- myapp/ViewController.swift
+++ myapp/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UITextField!
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func button(sender: AnyObject) {
+        println(text.text)
+        var age = text.text.toInt()
+        age = age! * 7
+       
+        label.text = "your cat is : \(age!)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
